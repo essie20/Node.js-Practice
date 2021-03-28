546f3151fcbdb35e0854b3dce93e1ff3f54cb776
P2948 <p2948@student.jamk.fi>
2021-03-28 23:43:11 +0300

    add initial code

---
+++ test/customer.js
@@ -0,0 +1,58 @@
+describe('Describe test cases', () => {
+    it('Description to one test case', (done) => {
+        
+        const chai = require('chai');
+        const chaihttp = require('chai-http');
+        const app = require('../index');
+        const query = require('../db/customers');
+        const should = chai.should();
+        
+        chai.use(chaihttp);
+
+        const testCustomer = {
+            firstname: 'John',
+            lastname: 'Johnson',
+            email: 'john@johnson.com',
+            phone: '8233243'
+          }
+                  
+        describe('/POST customers', () => {
+            beforeEach((done) => {
+                query.deleteAllCustomers();
+                done();
+            });
+
+          it('Add new customer', (done) => {
+            chai.request(app)
+            .post('/api/customers')
+            .set('Content-Type', 'application/json')
+            .send(JSON.stringify(testCustomer))
+            .end((err, res) => {
+               res.should.have.status(200); 
+               res.body.should.be.a('object');
+               res.body.should.have.property('firstname');
+               res.body.should.have.property('lastname');
+               res.body.should.have.property('email');
+               res.body.should.have.property('phone');
+               done();
+          });
+        });
+    });
+
+    describe('/GET customers', () => {
+        it('Fetch all customers', (done) => {
+          chai.request(app)
+            .get('/api/customers')
+            .end((err, res) => {
+              res.should.have.status(200);
+              res.body.should.be.a('array');
+              res.body.length.should.be.eql(1);
+              done();
+          });
+        });
+      });
+      
+    done();
+});
+});
+
