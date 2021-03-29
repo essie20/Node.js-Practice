a060baaf6fdc86eef391b9d8265283d8c7bf00f4
essie20 <ryghl683@gmail.com>
2021-03-29 08:16:52 +0300

    Route for the login

--- index.js
+++ index.js
@@ -2,11 +2,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const query = require('./db/customers');
+const auth = require('./services/authenticate');
 
 const app = express();
 app.use(bodyParser.json());
 
 const port = 3000;
+
+process.env.SECRET_KEY = "5b1a3923cc1e1e19523fd5c3f20b409509d3ff9d42710a4da095a2ce285b009f0c3730cd9b8e1af3eb84d";
+
 
 app.get("/api/customers", query.getAllCustomers);
 app.get("/api/customers/:id", query.getCustomerById);
@@ -14,6 +18,9 @@ app.post("/api/customers", query.addCustomer);
 app.delete("/api/customers/:id", query.deleteCustomer);
 app.delete("/api/customers", query.deleteAllCustomers);
 app.put("/api/customers/:id", query.updateCustomer);
+
+// Route for login
+app.post("/login", auth.login);
 
 app.listen(port, () => {{}
   console.log(`Server is running on port ${port}.`);
