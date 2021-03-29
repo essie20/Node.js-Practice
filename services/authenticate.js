3ce30b88c597fb4a9701c62faea38e79003282a2
essie20 <ryghl683@gmail.com>
2021-03-29 08:44:11 +0300

    Implementation of the authentication services

--- services/authenticate.js
+++ services/authenticate.js
@@ -2,26 +2,26 @@ const jwt = require('jsonwebtoken');
 const user = require('../db/users');
 const bcrypt = require('bcrypt');
 
+
 // User login
 const login = (req, res) => {
-  // Extract email and password from the request body
   const email = req.body.email;
   const password = req.body.password;
 
   const loginUser = user.getUserByEmail(email, (user) => {
-  if (user.length > 0) {
-    const hashpwd = user[0].password;
-    const token = jwt.sign({userId: email}, process.env.SECRET_KEY);
+    if (user.length > 0) {
+      const hashpwd = user[0].password;
+      const token = jwt.sign({userId: email}, process.env.SECRET_KEY);
 
-    if (bcrypt.compareSync(password, hashpwd))
-      res.send({token});
-    else
-      res.sendStatus(400).end(); 
-  }
-  else {
-    res.sendStatus(400).end();
-  }
-});
+      if (bcrypt.compareSync(password, hashpwd))
+        res.send({token});
+      else
+        res.sendStatus(400).end();
+    }
+    else {
+      res.sendStatus(400).end();
+    }
+  });
 }
 
 // User authentication
@@ -31,14 +31,14 @@ const authenticate = (req, res, next) => {
     res.sendStatus(400).end();
   }
   
-  // Verify the received token
   jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
     if (err)
-     res.sendStatus(400).end();
-   else
-     next();
-  }); 
-}
+      res.sendStatus(400).end();
+    else
+      next();
+  });
+} 
+
 
 module.exports = {
   authenticate: authenticate,
